d62f708a849fe7a4cb60681c71fdaedf114104b4
Randy Reddig <randy@alta.software>
2021-03-21 13:24:24 -0700

    Sources/Opus: reorganize public and private methods into extensions

--- Sources/Opus/Opus.Encoder.swift
+++ Sources/Opus/Opus.Encoder.swift
@@ -34,67 +34,75 @@ public extension Opus {
 				throw error
 			}
 		}
+	}
+}
 
-		public func encode(_ input: AVAudioPCMBuffer, to output: inout Data) throws -> Int {
-			output.count = try output.withUnsafeMutableBytes {
-				try encode(input, to: $0)
-			}
-			return output.count
-		}
+// MARK: Public encode methods
 
-		public func encode(_ input: AVAudioPCMBuffer, to output: inout [UInt8]) throws -> Int {
-			return try output.withUnsafeMutableBufferPointer {
-				try encode(input, to: $0)
-			}
+extension Opus.Encoder {
+	public func encode(_ input: AVAudioPCMBuffer, to output: inout Data) throws -> Int {
+		output.count = try output.withUnsafeMutableBytes {
+			try encode(input, to: $0)
 		}
+		return output.count
+	}
 
-		public func encode(_ input: AVAudioPCMBuffer, to output: UnsafeMutableRawBufferPointer) throws -> Int {
-			let umbp = UnsafeMutableBufferPointer(start: output.baseAddress!.bindMemory(to: UInt8.self, capacity: output.count), count: output.count)
-			return try encode(input, to: umbp)
+	public func encode(_ input: AVAudioPCMBuffer, to output: inout [UInt8]) throws -> Int {
+		return try output.withUnsafeMutableBufferPointer {
+			try encode(input, to: $0)
 		}
+	}
 
-		public func encode(_ input: AVAudioPCMBuffer, to output: UnsafeMutableBufferPointer<UInt8>) throws -> Int {
-			guard input.format.sampleRate == format.sampleRate, input.format.channelCount == format.channelCount else {
-				throw Opus.Error.badArg
-			}
-			switch format.commonFormat {
-			case .pcmFormatInt16:
-				let input = UnsafeBufferPointer(start: input.int16ChannelData![0], count: Int(input.frameLength * format.channelCount))
-				return try encode(input, to: output)
-			case .pcmFormatFloat32:
-				let input = UnsafeBufferPointer(start: input.floatChannelData![0], count: Int(input.frameLength * format.channelCount))
-				return try encode(input, to: output)
-			default:
-				throw Opus.Error.badArg
-			}
-		}
+	public func encode(_ input: AVAudioPCMBuffer, to output: UnsafeMutableRawBufferPointer) throws -> Int {
+		let umbp = UnsafeMutableBufferPointer(start: output.baseAddress!.bindMemory(to: UInt8.self, capacity: output.count), count: output.count)
+		return try encode(input, to: umbp)
+	}
 
-		public func encode(_ input: UnsafeBufferPointer<Int16>, to output: UnsafeMutableBufferPointer<UInt8>) throws -> Int {
-			let count = opus_encode(
-				encoder,
-				input.baseAddress!,
-				Int32(input.count) / Int32(format.channelCount),
-				output.baseAddress!,
-				Int32(output.count)
-			)
-			if count < 0 {
-				throw Opus.Error(count)
-			}
-			return Int(count)
+	public func encode(_ input: AVAudioPCMBuffer, to output: UnsafeMutableBufferPointer<UInt8>) throws -> Int {
+		guard input.format.sampleRate == format.sampleRate, input.format.channelCount == format.channelCount else {
+			throw Opus.Error.badArg
 		}
+		switch format.commonFormat {
+		case .pcmFormatInt16:
+			let input = UnsafeBufferPointer(start: input.int16ChannelData![0], count: Int(input.frameLength * format.channelCount))
+			return try encode(input, to: output)
+		case .pcmFormatFloat32:
+			let input = UnsafeBufferPointer(start: input.floatChannelData![0], count: Int(input.frameLength * format.channelCount))
+			return try encode(input, to: output)
+		default:
+			throw Opus.Error.badArg
+		}
+	}
+}
 
-		public func encode(_ input: UnsafeBufferPointer<Float32>, to output: UnsafeMutableBufferPointer<UInt8>) throws -> Int {
-			let count = opus_encode_float(
-				encoder,
-				input.baseAddress!,
-				Int32(input.count) / Int32(format.channelCount),
-				output.baseAddress!,
-				Int32(output.count)
-			)
-			if count < 0 {
-				throw Opus.Error(count)
-			}
-			return Int(count)
+// MARK: private encode methods
+
+extension Opus.Encoder {
+	private func encode(_ input: UnsafeBufferPointer<Int16>, to output: UnsafeMutableBufferPointer<UInt8>) throws -> Int {
+		let count = opus_encode(
+			encoder,
+			input.baseAddress!,
+			Int32(input.count) / Int32(format.channelCount),
+			output.baseAddress!,
+			Int32(output.count)
+		)
+		if count < 0 {
+			throw Opus.Error(count)
 		}
+		return Int(count)
+	}
+
+	private func encode(_ input: UnsafeBufferPointer<Float32>, to output: UnsafeMutableBufferPointer<UInt8>) throws -> Int {
+		let count = opus_encode_float(
+			encoder,
+			input.baseAddress!,
+			Int32(input.count) / Int32(format.channelCount),
+			output.baseAddress!,
+			Int32(output.count)
+		)
+		if count < 0 {
+			throw Opus.Error(count)
+		}
+		return Int(count)
 	}
 }
