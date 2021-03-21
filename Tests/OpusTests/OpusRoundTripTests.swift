fa0525d60d78c3668cd9d9684afd3c94d4ef018b
Randy Reddig <randy@alta.software>
2021-03-21 14:03:22 -0700

    Tests/OpusTests: janky round-trip tests comparing PCM buffers

--- Tests/OpusTests/OpusRoundTripTests.swift
+++ Tests/OpusTests/OpusRoundTripTests.swift
@@ -16,5 +16,19 @@ final class OpusRoundTripTests: XCTestCase {
 
 		let encodedSize = try encoder.encode(input, to: &data)
 		XCTAssertEqual(encodedSize, 14)
+
+		let output = try decoder.decode(data)
+
+		assertEqual(input, output)
+	}
+
+	func assertEqual(_ a: AVAudioPCMBuffer, _ b: AVAudioPCMBuffer, epsilon: Float32 = 0.00001) {
+		XCTAssertTrue(a.format.isEqual(b.format), "a.format == b.format")
+		XCTAssertEqual(a.frameLength, b.frameLength, "a.frameLength == b.frameLength")
+		for i in 0 ... a.frameLength {
+			let x = a.floatChannelData![0][Int(i)]
+			let y = b.floatChannelData![0][Int(i)]
+			XCTAssert(abs(x - y) < epsilon)
+		}
 	}
 }
