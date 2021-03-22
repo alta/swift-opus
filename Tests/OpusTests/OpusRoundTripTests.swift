b4f61660308dc444d24667dc42b2f70a00359a75
Randy Reddig <randy@alta.software>
2021-03-21 17:34:33 -0700

    Tests/OpusTests: play sounds in tests

--- Tests/OpusTests/OpusRoundTripTests.swift
+++ Tests/OpusTests/OpusRoundTripTests.swift
@@ -4,31 +4,73 @@ import XCTest
 @testable import Opus
 
 final class OpusRoundTripTests: XCTestCase {
+	let engine = AVAudioEngine()
+
 	func testSilence() throws {
 		let format = OpusTests.formatFloat32Mono
-		let encoder = try Opus.Encoder(format: format)
-		let decoder = try Opus.Decoder(format: format)
-
 		let input = AVAudioPCMBuffer(pcmFormat: format, frameCapacity: 5760)!
-		input.frameLength = input.frameCapacity
-
-		var data = Data(count: 1500)
-
-		let encodedSize = try encoder.encode(input, to: &data)
-		XCTAssertEqual(encodedSize, 14)
-
-		let output = try decoder.decode(data)
-
-		assertSimilar(input, output)
+		input.frameLength = input.frameCapacity // Silence
+		let output = try encodeAndDecode(input)
 	}
 
-	func assertSimilar(_ a: AVAudioPCMBuffer, _ b: AVAudioPCMBuffer, epsilon: Float32 = 0.00001) {
+	func testSoundFile() throws {
+		let url = Bundle.module.url(forResource: "MuteMono", withExtension: "wav")!
+		let audioFile = try AVAudioFile(forReading: url)
+		let format = OpusTests.formatFloat32Mono
+		let input = AVAudioPCMBuffer(pcmFormat: format, frameCapacity: 5760)!
+		try audioFile.read(into: input, frameCount: input.frameCapacity)
+		let output = try encodeAndDecode(input)
+	}
+
+	func encodeAndDecode(_ input: AVAudioPCMBuffer) throws -> AVAudioPCMBuffer {
+		let encoder = try Opus.Encoder(format: input.format)
+		let decoder = try Opus.Decoder(format: input.format)
+		var data = Data(count: 1500)
+		try encoder.encode(input, to: &data)
+		let output = try decoder.decode(data)
+
+		try play(input)
+		Thread.sleep(forTimeInterval: 0.25)
+		try play(output)
+		Thread.sleep(forTimeInterval: 0.25)
+
+		// assertSimilar(input, output)
+		return output
+	}
+
+	func play(_ buffer: AVAudioPCMBuffer) throws {
+		_ = engine.mainMixerNode
+		try engine.start()
+		let player = AVAudioPlayerNode()
+		engine.attach(player)
+		engine.connect(player, to: engine.mainMixerNode, format: buffer.format)
+		player.play()
+		player.scheduleBuffer(buffer) {
+			DispatchQueue.main.async {
+				self.engine.detach(player)
+			}
+		}
+		Thread.sleep(forTimeInterval: Double(buffer.frameLength) / Double(buffer.format.sampleRate))
+	}
+
+	func assertSimilar(_ a: AVAudioPCMBuffer, _ b: AVAudioPCMBuffer, epsilon: Float32 = 0.2) {
 		XCTAssertTrue(a.format.isEqual(b.format), "a.format == b.format")
 		XCTAssertEqual(a.frameLength, b.frameLength, "a.frameLength == b.frameLength")
 		for i in 0 ... a.frameLength {
-			let x = a.floatChannelData![0][Int(i)]
-			let y = b.floatChannelData![0][Int(i)]
-			XCTAssert(abs(x - y) < epsilon)
+			var x: Float32 = 0
+			var y: Float32 = 0
+			switch a.format.commonFormat {
+			case .pcmFormatInt16:
+				x = Float32(a.int16ChannelData![0][Int(i)]) / 32768.0
+				y = Float32(b.int16ChannelData![0][Int(i)]) / 32768.0
+			case .pcmFormatFloat32:
+				x = a.floatChannelData![0][Int(i)]
+				y = b.floatChannelData![0][Int(i)]
+			default:
+				XCTFail("unknown audio format: \(a.format)")
+			}
+			let delta = abs(abs(x) - abs(y))
+			XCTAssert(delta < epsilon, String(delta))
 		}
 	}
 }
