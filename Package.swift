b41d414268b9ee82c50f961e197f11da6acef344
Emlyn Bolton <emlyn.bolton@mac.com>
2022-04-19 12:11:16 -0700

    Tidy some changes prior to PR back to fork source

--- Package.swift
+++ Package.swift
@@ -31,9 +31,9 @@ let package = Package(
 			exclude: [
 				"AUTHORS",
 				"autogen.sh",
-				"celt/arm",
 				"celt_headers.mk",
 				"celt_sources.mk",
+				"celt/arm",
 				"celt/dump_modes",
 				"celt/meson.build",
 				"celt/opus_custom_demo.c",
@@ -91,7 +91,7 @@ let package = Package(
 				.headerSearchPath("silk"),
 				.headerSearchPath("silk/float"),
 
-        .define("OPUS_BUILD", to: "1"),
+				.define("OPUS_BUILD"),
 				.define("CUSTOM_MODES"),
 				.define("VAR_ARRAYS", to: "1"),
 				.define("FLOATING_POINT"), // Enable Opus floating-point mode
